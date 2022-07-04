be2d450848bf4dc07647021ae30900c687d444c7
Ferry Aryadicka <93524589+faryadicka@users.noreply.github.com>
2022-07-04 18:50:48 +0700

    Delete .history directory

--- .history/src/modules/topup_20220704165107.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-const HOST = `https://fazzpay.herokuapp.com`
-const { NEXT_PUBLIC_BASE_URL } = process.env
-
-
-export const postTopUpAxios = (body, token) => {
-  const URL = `${NEXT_PUBLIC_BASE_URL}/transaction/top-up`
-  return axios.post(URL, body, { headers: { 'Authorization': `Bearer ${token}` } })
-}
--- .history/src/modules/topup_20220704165111.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-const { NEXT_PUBLIC_BASE_URL } = process.env
-
-
-export const postTopUpAxios = (body, token) => {
-  const URL = `${NEXT_PUBLIC_BASE_URL}/transaction/top-up`
-  return axios.post(URL, body, { headers: { 'Authorization': `Bearer ${token}` } })
-}
