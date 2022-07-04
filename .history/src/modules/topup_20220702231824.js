7c19e0505326e178ebba21bd20a63697693ede07
faryadicka <faryadicka98@gmail.com>
2022-07-04 18:51:37 +0700

    delete folder.history

--- .history/src/modules/topup_20220702231824.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-const HOST = `https://fazzpay.herokuapp.com`
-
-export const postTopUpAxios = (body, token) => {
-  const URL = `${HOST}/transaction/top-up`
-  return axios.post(URL, body, { headers: { 'Authorization': `Bearer ${token}` } })
-}
